4e345e24b6e31729968182aeeb23cf35d4cd6a13
anshulprojectmgmt <anshul.projectmgmt@gmail.com>
2025-12-05 15:26:13 +0530

    Update routing to use ResultsPage instead of PreviewPage
    Replaced PreviewPage with ResultsPage in routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+// import { Routes, Route } from 'react-router-dom'
+// import LandingPage from './pages/LandingPage'
+// import UploadPage from './pages/UploadPage'
+// import PreviewPage from './pages/PreviewPage'
+// import NotFoundPage from './pages/NotFoundPage'
+// import VideoEditorPage from './pages/VideoEditorPage'
+// import Layout from './components/Layout'
+// import WelcomePopup from './components/WelcomePopup'
+// import './App.css'
+
+// function App() {
+//   return (
+//     <Layout>
+//       {/* <WelcomePopup /> */}
+//       <Routes>
+//         <Route path="/" element={<LandingPage />} />
+//         <Route path="/video-enhancer" element={<UploadPage />} />
+//         <Route path="/preview" element={<PreviewPage />} />
+//         <Route path="/videoeditor" element={<VideoEditorPage />} />
+//         <Route path="*" element={<NotFoundPage />} />
+//       </Routes>
+//     </Layout>
+//   )
+// }
+
+// export default App
+
+
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import UploadPage from './pages/UploadPage'
-import PreviewPage from './pages/PreviewPage'
+// import PreviewPage from './pages/PreviewPage' <--- DELETE THIS LINE
+import ResultsPage from './pages/ResultsPage' // <--- ADD THIS LINE
 import NotFoundPage from './pages/NotFoundPage'
 import VideoEditorPage from './pages/VideoEditorPage'
 import Layout from './components/Layout'
-import WelcomePopup from './components/WelcomePopup'
+// import WelcomePopup from './components/WelcomePopup'
 import './App.css'
 
 function App() {
@@ -15,7 +44,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/video-enhancer" element={<UploadPage />} />
-        <Route path="/preview" element={<PreviewPage />} />
+        
+        {/* UPDATE THIS ROUTE TO USE YOUR NEW PAGE */}
+        <Route path="/preview" element={<ResultsPage />} /> 
+        
         <Route path="/videoeditor" element={<VideoEditorPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
