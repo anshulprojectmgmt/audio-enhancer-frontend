b8aa0f9a8833c4ac29c7c96f1d311d2166f4c4d2
anshulprojectmgmt <anshul.projectmgmt@gmail.com>
2025-12-05 15:33:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,11 @@
-// import { Routes, Route } from 'react-router-dom'
-// import LandingPage from './pages/LandingPage'
-// import UploadPage from './pages/UploadPage'
-// import PreviewPage from './pages/PreviewPage'
-// import NotFoundPage from './pages/NotFoundPage'
-// import VideoEditorPage from './pages/VideoEditorPage'
-// import Layout from './components/Layout'
-// import WelcomePopup from './components/WelcomePopup'
-// import './App.css'
-
-// function App() {
-//   return (
-//     <Layout>
-//       {/* <WelcomePopup /> */}
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/video-enhancer" element={<UploadPage />} />
-//         <Route path="/preview" element={<PreviewPage />} />
-//         <Route path="/videoeditor" element={<VideoEditorPage />} />
-//         <Route path="*" element={<NotFoundPage />} />
-//       </Routes>
-//     </Layout>
-//   )
-// }
-
-// export default App
-
-
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import UploadPage from './pages/UploadPage'
-// import PreviewPage from './pages/PreviewPage' <--- DELETE THIS LINE
-import ResultsPage from './pages/ResultsPage' // <--- ADD THIS LINE
+import PreviewPage from './pages/PreviewPage'
 import NotFoundPage from './pages/NotFoundPage'
 import VideoEditorPage from './pages/VideoEditorPage'
 import Layout from './components/Layout'
-// import WelcomePopup from './components/WelcomePopup'
+import WelcomePopup from './components/WelcomePopup'
 import './App.css'
 
 function App() {
@@ -44,10 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/video-enhancer" element={<UploadPage />} />
-        
-        {/* UPDATE THIS ROUTE TO USE YOUR NEW PAGE */}
-        <Route path="/preview" element={<ResultsPage />} /> 
-        
+        <Route path="/preview" element={<PreviewPage />} />
         <Route path="/videoeditor" element={<VideoEditorPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
@@ -56,3 +24,35 @@ function App() {
 }
 
 export default App
+
+
+// import { Routes, Route } from 'react-router-dom'
+// import LandingPage from './pages/LandingPage'
+// import UploadPage from './pages/UploadPage'
+// // import PreviewPage from './pages/PreviewPage' <--- DELETE THIS LINE
+// import ResultsPage from './pages/ResultsPage' // <--- ADD THIS LINE
+// import NotFoundPage from './pages/NotFoundPage'
+// import VideoEditorPage from './pages/VideoEditorPage'
+// import Layout from './components/Layout'
+// // import WelcomePopup from './components/WelcomePopup'
+// import './App.css'
+
+// function App() {
+//   return (
+//     <Layout>
+//       {/* <WelcomePopup /> */}
+//       <Routes>
+//         <Route path="/" element={<LandingPage />} />
+//         <Route path="/video-enhancer" element={<UploadPage />} />
+        
+//         {/* UPDATE THIS ROUTE TO USE YOUR NEW PAGE */}
+//         <Route path="/preview" element={<ResultsPage />} /> 
+        
+//         <Route path="/videoeditor" element={<VideoEditorPage />} />
+//         <Route path="*" element={<NotFoundPage />} />
+//       </Routes>
+//     </Layout>
+//   )
+// }
+
+// export default App
